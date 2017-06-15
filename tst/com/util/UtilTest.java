d3d01513340671980ca7173ca236b7b6a81efc4a
Stefan Walesch <wallinator@gmx.de>
2017-06-15 22:24:28 +0200

    use assertj for tests

--- tst/com/util/UtilTest.java
+++ tst/com/util/UtilTest.java
@@ -1,14 +1,10 @@
 package com.util;
 
 import static java.util.Arrays.asList;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 
-import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
 import org.junit.Test;
 
 public class UtilTest {
@@ -16,20 +12,20 @@ public class UtilTest {
 	public void findsSmallestIntegerPosition() {
 		List<Integer> input = asList(5, 4, 2, 42, 12);
 		int position = Util.getIdOfLowestValue(input);
-		assertEquals(2, position);
+		assertThat(position).isEqualTo(2);
 	}
-	
+
 	@Test
 	public void findsHighestIntegerPosition() {
 		List<Integer> input = asList(5, 4, 2, 42, 12);
 		int position = Util.getIdOfHighestValue(input);
-		assertEquals(3, position);
+		assertThat(position).isEqualTo(3);
 	}
-	
+
 	@Test
-	public void splittsNumberSringToList(){
+	public void splittsNumberSringToList() {
 		String input = "0:8:15";
 		List<Integer> result = Util.convertStringToList(input, ":");
-		assertThat(result, is(asList(0,8,15)));
+		assertThat(result).containsExactlyElementsOf(asList(0, 8, 15));
 	}
 }
